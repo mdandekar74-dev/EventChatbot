8129c0db62e505ab9f0f3db33b01c4c689f0ddf5
mdandekar74-dev <mdandekar74@gmail.com>
2025-11-15 19:24:12 +0000

    updated app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,16 @@
 import streamlit as st
 from openai import OpenAI
-
 import os
-
-from openai import OpenAI
 from openai.types.responses import ResponseOutputMessage
+
+#######################
+
+########################
 
 def find_ticketmaster_events(input_text):
   print("In find_ticketmaster_events")
-  client = OpenAI()
-
+  
+  # Call the OpenAI API with the MCP tool to get Ticketmaster events
   resp = client.responses.create(
       model="gpt-4.1-mini",
       tools=[
@@ -33,18 +34,75 @@ def find_ticketmaster_events(input_text):
   # Join them for display
   st.write("\n\n".join(user_texts))
 
+###############
+def get_hotel_info(input_prompt):
 
+    st.write("Getting hotel info...")
+    
+    resp = client.responses.create(
+       model="gpt-4.1-mini",
+       input=input_prompt
+       )
+
+    user_texts = []
+    for item in resp.output:
+         if isinstance(item, ResponseOutputMessage):
+          for c in item.content:
+              if hasattr(c, "text"):
+                  user_texts.append(c.text)
+
+       # Join them for display
+    st.write("\n\n".join(user_texts))
+
+    user_response = st.text_input("would you like to search for some sightseeing places in the city? yes/no ")
+
+    if user_response=="yes":
+        info_prompt = "find sightseeing places in " + user_destination_city
+        st.write("Getting more info...")
+    
+        resp = client.responses.create(
+           model="gpt-4.1-mini",
+           input=info_prompt
+           )
+
+        user_texts = []
+        for item in resp.output:
+             if isinstance(item, ResponseOutputMessage):
+              for c in item.content:
+                  if hasattr(c, "text"):
+                      user_texts.append(c.text)
+
+         # Join them for display
+        st.write("\n\n".join(user_texts))
+
+###############
+def get_flight_info(input_prompt):
+    st.write("Getting flight info...")
+    print(input_prompt)
+    resp=client.responses.create( model="gpt-4.1-mini",
+    input=input_prompt)
+
+    user_texts=[]
+    for item in resp.output:
+      if isinstance(item, ResponseOutputMessage):
+          for c in item.content:
+              if hasattr(c, "text"):
+                  user_texts.append(c.text)
+
+    # Join them for display
+    st.write("\n\n".join(user_texts))
+    
+
+##########
 print("At the start")
 os.environ['OPENAI_API_KEY']=st.secrets['OPENAI_API_KEY']
-
-
+client = OpenAI()
 
 # Show title and description.
 st.title("Smart Event Planner Chatbot")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "This is a simple chatbot that uses OpenAI's GPT-4.1-mini model to generate responses. "    
+    "It can help you find music events and nearby hotels based on your preferences." 
 )
 
 # Create a session state variable to store the chat messages. This ensures that the
@@ -59,20 +117,27 @@ for message in st.session_state.messages:
 
 # Create a chat input field to allow the user to enter a message. This will display
 # automatically at the bottom of the page.
-user_city = st.text_input("Enter the city for music events:", key="city")
+user_destination_city = st.text_input("Enter the city for music events:", key="city")
 user_timeline = st.text_input("Enter the month and year:", key="timeline")
-user_music_genre = st.text_input("Enter a genre (or 'none'):", key="genre")
-if user_city:
-    print(f"user city is {user_city}")
+user_music_genre = st.text_input("Enter a genre (or 'any'):", key="genre")
 
-if user_timeline:
-    print(f"user timeline is {user_timeline}")
-if user_music_genre:
-    print(f"user music genre is {user_music_genre}")
-tm_input = "Find music events in city " + user_city + " for genre " + user_music_genre + " for " + user_timeline
-print(f"user prompt is {tm_input}")
 
-if user_city and user_timeline and user_music_genre:
-    st.write("Initiating event search...")
-    find_ticketmaster_events(tm_input)
-    
+if user_destination_city and user_timeline and user_music_genre:
+  tm_input = "Find music events in city " + user_destination_city + " for genre " + user_music_genre + " for " + user_timeline
+  print(f"user prompt is {tm_input}")
+   
+  st.write("Initiating music event search...")
+  find_ticketmaster_events(tm_input)
+
+  #user_eventDate = st.text_input("Please enter the date for the music event you would like to attend ")
+  #if user_eventDate:
+  user_eventVenue = st.text_input("Please enter the venue for the music event you would like to attend")
+  if user_eventVenue:      
+    input_prompt = "find hotels for city " + user_destination_city + " near " + user_eventVenue
+    print(input_prompt)
+    get_hotel_info(input_prompt)
+ 
+       
+
+
+
